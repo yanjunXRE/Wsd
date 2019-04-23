5b52b28f415f38990324d09b9f44230125b46b07
yanjunXRE <49858946+yanjunXRE@users.noreply.github.com>
2019-04-23 16:59:35 +0800

    Add files via upload

---
+++ PA1/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PA1.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PA1.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
