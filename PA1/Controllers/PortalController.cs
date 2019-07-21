7deb8bda87c7925b4609b429b2eff72e42880c4c
tan_yan_jun3@connect.ite.edu.sg <tan_yan_jun3@connect.ite.edu.sg>
2019-07-21 17:13:30 +0800

    V3

--- PA1/Controllers/PortalController.cs
+++ PA1/Controllers/PortalController.cs
@@ -9,19 +9,20 @@ namespace PA1.Controllers
 {
     public class PortalController : Controller
     {
+       
         // GET: Portal 
         DataContext db = new DataContext();
         public ActionResult Index()
         {
-            var data = db.Order.SqlQuery("SELECT * FROM Order").ToList();
+            var data = db.Order.SqlQuery("SELECT * FROM Orders").ToList();
             return View(data);
         }
-       
 
         // GET: Portal/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var data = db.Order.SqlQuery("Select * From Orders where OrderID=@p0", id).SingleOrDefault();
+            return View(data);
         }
 
         // GET: Portal/Create
@@ -32,13 +33,26 @@ namespace PA1.Controllers
 
         // POST: Portal/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Order collection)
         {
             try
             {
                 // TODO: Add insert logic here
+                List<object> newRecord = new List<object>();
 
-                return RedirectToAction("Index");
+                newRecord.Add(collection.FoodDescription);
+                newRecord.Add(collection.DeliveryAddress);
+                newRecord.Add(collection.DeliveryDate);
+                newRecord.Add(collection.DeliveryTime);
+                newRecord.Add(collection.EmailAddress);
+                newRecord.Add(collection.ContactNumber);
+                object[] recordItem = newRecord.ToArray();
+                int result = db.Database.ExecuteSqlCommand("INSERT INTO orders " + "(FoodDescription,DeliveryAddress,DeliveryDate,DeliveryTime,EmailAddress,ContactNumber)" + "VALUES(@p0,@p1,@p2,@p3,@p4,@p5)", recordItem);
+                if (result > 0)
+                {
+                    ViewBag.msg = " Orders record is added";
+                }
+                return View();
             }
             catch
             {
@@ -46,32 +60,47 @@ namespace PA1.Controllers
             }
         }
 
-        // GET: Portal/Edit/5
+        // GET: Portal/Create
         public ActionResult Edit(int id)
         {
-            return View();
+            var data = db.Order.SqlQuery("SELECT * FROM Orders WHERE OrderID =@p0", id).SingleOrDefault();
+            return View(data);
         }
 
-        // POST: Portal/Edit/5
+        // POST: Portal/Create
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id,Order collection)
         {
             try
             {
-                // TODO: Add update logic here
+                // TODO: Add insert logic here
+                List<object> newRecord = new List<object>();
 
-                return RedirectToAction("Index");
+                newRecord.Add(collection.FoodDescription);
+                newRecord.Add(collection.DeliveryAddress);
+                newRecord.Add(collection.DeliveryDate);
+                newRecord.Add(collection.DeliveryTime);
+                newRecord.Add(collection.EmailAddress);
+                newRecord.Add(collection.ContactNumber);
+                object[] recordItem = newRecord.ToArray();
+                
+                int result = db.Database.ExecuteSqlCommand("UPDATE orders" + " SET FoodDescription=@p0,DeliveryAddress=@p1,DeliveryDate=@p2,DeliveryTime=@p3,EmailAddress=@p4,ContactNumber=@p5 " + "WHERE OrderID=" + id, recordItem);
+                if (result > 0)
+                {
+                    ViewBag.msg = " Orders record is updated";
+                }
+                return View();
             }
             catch
             {
                 return View();
             }
         }
-
         // GET: Portal/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var data = db.Order.SqlQuery("SELECT * FROM Orders WHERE OrderID=@p0", id).SingleOrDefault();
+            return View(data);
         }
 
         // POST: Portal/Delete/5
@@ -81,8 +110,13 @@ namespace PA1.Controllers
             try
             {
                 // TODO: Add delete logic here
+                int result = db.Database.ExecuteSqlCommand("Delete FROM Orders WHERE  OrderID=@p0", id);
+                if (result > 0)
+                {
 
-                return RedirectToAction("Index");
+                    return RedirectToAction("Index");
+                }
+                return View();
             }
             catch
             {
--- PA1/Models/order.cs
+++ PA1/Models/order.cs
@@ -7,18 +7,19 @@ using System.Web;
 
 namespace PA1.Models
 {
-    [Table("Order")]
-    public class Order
-    {
-        [Key]
-        public int OrderID { get; set; }
-        public string Food_description { get; set; }
-        public string Delivery_address { get; set; }
-        public string Delivery_date { get; set; }
-        public string Delivery_time { get; set; }
-        public string Email_address { get; set; }
-        public string Contact_number { get; set; }
-        public string Order_status { get; set; }
-      
+ 
+        [Table("Orders")]
+        public class Order
+        {
+            [Key]
+            public int OrderID { get; set; }
+            public string FoodDescription { get; set; }
+            public string DeliveryAddress { get; set; }
+            public string DeliveryDate { get; set; }
+            public string DeliveryTime { get; set; }
+            public string EmailAddress { get; set; }
+            public string ContactNumber { get; set; }
+            public string OrderStatus { get; set; }
+
+        }
     }
-}
